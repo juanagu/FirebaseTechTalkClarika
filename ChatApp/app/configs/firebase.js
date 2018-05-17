c76128850eb934d24afb05306de6523cf829cb74
Juan Ignacio Agu <jagu@clarika.com.ar>
2018-05-17 11:36:43 -0300

    remove firebase config

--- ChatApp/app/configs/firebase.js
+++ ChatApp/app/configs/firebase.js
@@ -1,8 +1,2 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyAk9M4zP-tXvNnPKs3YuyFP8gRmGpEdjuE",
-    authDomain: "clarikachatapp.firebaseapp.com",
-    databaseURL: "https://clarikachatapp.firebaseio.com",
-    projectId: "clarikachatapp",
-    storageBucket: "",
-    messagingSenderId: "1065237643409"
 };
