bf3a134daf44bfc029e52e7cd8d4ecbd452d8aff
Juan Ignacio Agu <jagu@clarika.com.ar>
2018-05-29 23:02:25 -0300

    first commit kiosko app

--- ChatApp/app/configs/firebase.js
+++ ChatApp/app/configs/firebase.js
@@ -1,2 +1,8 @@
 export const firebaseConfig = {
+  apiKey: "AIzaSyAk9M4zP-tXvNnPKs3YuyFP8gRmGpEdjuE",
+    authDomain: "clarikachatapp.firebaseapp.com",
+    databaseURL: "https://clarikachatapp.firebaseio.com",
+    projectId: "clarikachatapp",
+    storageBucket: "",
+    messagingSenderId: "1065237643409"
 };
